3f157b0f4b911812e387f00ba78f33e9d9e90197
Markki Meyer <markkimeyer@gmail.com>
2021-04-11 10:56:52 -0500

    deploying

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(require("../Develop/routes/htmlRoutes"));
+
+app.use(require("../Develop/routes/apiRoutes/index.js"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
