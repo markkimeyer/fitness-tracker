95896ec195a2501d28d3b3342a598c70294461ea
Markki Meyer <markkimeyer@gmail.com>
2021-04-11 11:10:12 -0500

    update routes path

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use(require("../Develop/routes/htmlRoutes"));
+app.use(require("./Develop/routes/htmlRoutes"));
 
-app.use(require("../Develop/routes/apiRoutes/index.js"));
+app.use(require("./Develop/routes/apiRoutes"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { 
   useNewUrlParser: true,
